692124bc60db79bdcf748a69b384f44a3aa88e18
nathan-moura55 <nathan.lucas@iebtinnovation.com>
2025-02-24 15:56:36 -0300

    first commit

---
+++ src/components/login/login/login.jsx
@@ -0,0 +1,50 @@
+import { FaUser, FaLock } from 'react-icons/fa';
+import { useState } from 'react';
+import "./login.css";
+
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("enviando os dados: e-mail: " + username + " - senha: " + password);
+    };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <div className="input-field">
+                    <input type="email"
+                        placeholder="E-mail"
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <FaUser className="icon" />
+                </div>
+                <div className="input-field">
+                    <input type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)} />
+                    <FaLock className="icon" />
+                </div>
+
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox" />
+                        Lembre de mim
+                    </label>
+                    <a href="#"> Esqueceu a senha?</a>
+                </div>
+                <button>Entrar </button>
+                <div className="signup-link">
+                    <p>
+                        Não tem uma conta? <a href="#">Registrar</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login
---
+++ src/components/login/message/message.jsx
@@ -0,0 +1,11 @@
+import './message.css';
+
+const Message = () => {
+    return (
+        <div className="container-message">
+            <h1>Parabéns, você está no sistema!</h1>
+        </div>
+    );
+};
+
+export default Message
