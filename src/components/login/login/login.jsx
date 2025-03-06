434864fbf477c16d71cc0f673d3c11b2547dfb7e
nathan-moura55 <nathan.lucas@iebtinnovation.com>
2025-03-06 14:03:40 -0300

    remember me

--- src/components/login/login/login.jsx
+++ src/components/login/login/login.jsx
@@ -1,5 +1,5 @@
 import { FaUser, FaLock } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./login.css";
 import { useNavigate } from 'react-router-dom';
 import '../../../App.css'
@@ -8,24 +8,50 @@ const Login = () => {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const storedUsername = localStorage.getItem('username');
+        if (storedUsername) {
+            setUsername(storedUsername);
+        }
+    }, []);
+
     const handleLogin = () => {
+
+        console.log('Tentando login...');
+        console.log('Usuário:', username);
+        console.log('Senha:', password);
+
         if (username === 'admin' && password === 'batatinha123') {
+            console.log('Login bem-sucedido!');
+
+            if (rememberMe) {
+                localStorage.setItem('username', username);
+                localStorage.setItem('rememberMe', 'true');
+            } else {
+                localStorage.removeItem('username');
+                localStorage.setItem('rememberMe', 'true');
+            }
+
             navigate('/message');
         } else {
             alert('Usuário ou senha inválidos');
+            console.log('Credenciais inválidas');
         };
     };
     return (
         <div className="container">
             <form onSubmit={handleLogin}>
                 <h1>Login</h1>
+
                 <div className="input-field">
                     <input type="username"
                         placeholder="Username"
                         onChange={(e) => setUsername(e.target.value)} />
                     <FaUser className="icon" />
+
                 </div>
                 <div className="input-field">
                     <input type="password"
@@ -36,16 +62,25 @@ const Login = () => {
 
                 <div className="recall-forget">
                     <label>
-                        <input type="checkbox" />
+                        <input type="checkbox"
+                            checked={rememberMe}
+                            onChange={() => setRememberMe(!rememberMe)} />
                         Lembre de mim
                     </label>
                     <a href="#"> Esqueceu a senha?</a>
+
                 </div>
-                <button onClick={handleLogin}>Entrar</button>
-                <div className="signup-link">
+                <button
+                    onClick={handleLogin}
+                >Entrar
+                </button>
+
+                <div
+                    className="signup-link">
                     <p>
                         Não tem uma conta? <a href="#">Registrar</a>
                     </p>
+
                 </div>
             </form>
         </div>
